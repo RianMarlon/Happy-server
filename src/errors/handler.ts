9a49be998e49f1fd099cc9ee2a618cbc52afadcf
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-15 18:21:50 -0300

    Adicionando o errorHandler no server.ts no projeto server

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,11 +1,11 @@
-import { ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   console.log(error);
 
   return response.status(500).json({
     messageError: 'Erro no servidor!'
-  })
+  });
 }
 
 export default errorHandler;

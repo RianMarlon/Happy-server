544ed4ce7a20a55294d4a24978c1e07ecba82e3d
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-15 19:59:42 -0300

    Adicionando o yup para validação de erros na função create do OrphanagesController.ts no projeto server

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 
 import Orphanage from '../models/Orphanage';
 import orphanagesView from '../views/orphanagesView';
@@ -25,8 +26,8 @@ export default {
         path: image.filename
       }
     });
-  
-    const orphanage = orphanagesRepository.create({
+
+    const data = {
       name,
       latitude,
       longitude,
@@ -35,9 +36,30 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(500),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
     });
   
-    await orphanagesRepository.save(orphanage)
+    const orphanage = orphanagesRepository.create({ ...data });
+  
+    await orphanagesRepository.save(orphanage);
     
     return response.status(201).json(orphanage);
   },
@@ -65,8 +87,8 @@ export default {
     }
 
     catch(e) {
-      return response.status(200).json({
-        messageError: 'Nenhum orfanato encontrado!'
+      return response.status(400).json({
+        messageError: 'No orphanages found!'
       });
     }
   }

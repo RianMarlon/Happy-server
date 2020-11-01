5c1ad0d5311380996737477a82dc4c009018792e
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-01 10:52:10 -0300

    Adicionando o model User no projeto server

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export default class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: 0 })
+  verified_email: boolean;
+}

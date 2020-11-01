1d9b1b2ed01e0176fa16f242b8a1caf0095dc419
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-01 11:13:33 -0300

    Adicionando o UsersController no projeto server

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
+import encryptPassword from '../utils/encryptPassword';
+
+export default {
+  async create (request: Request, response: Response) {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      confirm_password,
+    } = request.body;
+
+    const usersRepository = getRepository(User);
+
+    const data = {
+      first_name,
+      last_name,
+      email,
+      password,
+      confirm_password,
+    }
+
+    const schema = Yup.object().shape({
+      first_name: Yup.string().required('Nome não informado!'),
+      last_name: Yup.string().required('Sobrenome não informado!'),
+      email: Yup.string().required('E-mail não informado!')
+        .email('E-mail inválido!'),
+      password: Yup.string().required('Senha não informada!')
+        .min(6, 'Senha deve conter, no mínimo, 6 caracteres'),
+      confirm_password: Yup.string().required('Senha de confirmação não informada!')
+        .equals([password], 'Senhas não conferem!')
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const userByEmail = await usersRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email')
+      .setParameters({
+        email
+      })
+      .getOne();
+
+    if (userByEmail) {
+      throw new Yup.ValidationError(
+        'E-mail informado já foi cadastrado!',
+        null, ''
+      );
+    }
+
+    const passwordEncrypted = encryptPassword(password);
+
+    const newData = {
+      first_name,
+      last_name,
+      email,
+      password: passwordEncrypted,
+    }
+
+    const user = usersRepository.create({ ...newData });
+
+    await usersRepository.save(user);
+
+    return response.status(201).json();
+  },
+}

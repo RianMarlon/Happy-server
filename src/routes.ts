2470c3be7aaec30d567e104dd4070cf3ddd40f4e
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-04 15:00:53 -0300

    Adicionando rota para criação de usuário no routes.ts do projeto server

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import OrphanagesController from './controllers/OrphanagesController';
+import UsersController from './controllers/UsersController';
+
 import uploadConfig from './config/upload';
 
 const routes = Router();
 const upload = multer(uploadConfig);
+
+routes.post('/signup', UsersController.create);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);

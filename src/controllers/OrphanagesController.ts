f9c9e2527206c16d0c5d758089d694a678892183
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-26 02:04:48 -0300

    Adicionando a função indexPending no OrphanagesController do projeto server

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -124,6 +124,64 @@ export default {
     return response.status(200).json(orphanagesView.renderMany(orphanages));
   },
 
+  async indexConfirmed(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const filters = request.query;
+
+    const page = Number(filters.page as string) || 1;
+    const perPage = Number(filters.per_page as string) || 1;
+
+    const limit = perPage;
+    const offset = perPage * (page - 1);
+
+    const quantityOrphanagesConfirmed = await orphanagesRepository
+      .count({ confirmed: true });
+  
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+      skip: offset,
+      take: limit,
+      where: {
+        confirmed: true
+      }
+    });
+
+    return response.status(200).json({
+      orphanages_by_page: orphanagesView.renderMany(orphanages),
+      quantity_confirmed: quantityOrphanagesConfirmed
+    });
+  },
+
+  async indexPending(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const filters = request.query;
+
+    const page = Number(filters.page as string) || 1;
+    const perPage = Number(filters.per_page as string) || 1;
+
+    const limit = perPage;
+    const offset = perPage * (page - 1);
+
+    const quantityOrphanagesPending = await orphanagesRepository
+      .count({ confirmed: false });
+  
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+      skip: offset,
+      take: limit,
+      where: {
+        confirmed: false
+      }
+    });
+
+    return response.status(200).json({
+      orphanages: orphanagesView.renderMany(orphanages),
+      quantity_pending: quantityOrphanagesPending
+    });
+  },
+
   async show(request: Request, response: Response) {
     const { id } = request.params;
     const orphanagesRepository = getRepository(Orphanage);
@@ -288,6 +346,62 @@ export default {
       const image = imagesRepository.create(newImages);
       await imagesRepository.save(image);
     }
+
+    return response.status(204).json();
+  },
+
+  async destroy(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanageById = await orphanagesRepository.findOne({
+      relations: ['images'],
+      where: {
+        id
+      }
+    });
+
+    if (!orphanageById) {
+      return response.status(400).json({
+        messagesError: ['Nenhum orfanato encontrado!']
+      });
+    }
+
+    await orphanagesRepository
+      .createQueryBuilder()
+      .delete()
+      .where('id = :id')
+      .setParameters({
+        id
+      })
+      .execute();
+
+    return response.status(200).json();
+  },
+
+  async confirm(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanageById = await orphanagesRepository.findOne({
+      relations: ['images'],
+      where: {
+        id
+      }
+    });
+
+    if (!orphanageById) {
+      return response.status(400).json({
+        messagesError: ['Nenhum orfanato encontrado!']
+      });
+    }
+
+    await orphanagesRepository
+      .update(id, {
+        confirmed: true
+      });
 
     return response.status(204).json();
   }

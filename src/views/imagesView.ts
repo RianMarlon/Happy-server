cdf0fda4f1dbd1d605e5867fcf4bc581e512d786
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-28 12:43:35 -0300

    Adicionando o process.env.URL_BACKEND no render do imagesView do projeto server

--- src/views/imagesView.ts
+++ src/views/imagesView.ts
@@ -1,10 +1,11 @@
 import Image from '../models/Image';
+import 'dotenv/config';
 
 export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://192.168.1.6:3333/uploads/${image.path}`,
+      url: `${process.env.URL_BACKEND}/uploads/${image.path}`,
     };
   },
 

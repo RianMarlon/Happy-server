0c29fb9bffa6d4ee08ec693bc9bc1718fcb01e4e
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-31 16:53:43 -0300

    Renomeando o atributo URL_BACKEND para API_URL no .env_file do projeto server

--- src/views/imagesView.ts
+++ src/views/imagesView.ts
@@ -5,7 +5,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `${process.env.URL_BACKEND}/uploads/${image.path}`,
+      url: `${process.env.API_URL}/uploads/${image.path}`,
     };
   },
 

4f6f63bbe758848c7f1fc8734d9f1819e6972b11
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-31 14:48:26 -0300

    Adicionando o create-users.seed.ts para a inserção do primeiro usuário administrador no projeto server

---
+++ src/database/seeds/create-users.seed.ts
@@ -0,0 +1,24 @@
+import { Connection } from "typeorm";
+import { Factory, Seeder } from "typeorm-seeding";
+
+import User from "../../models/User";
+
+import encryptPassword from "../../utils/encryptPassword";
+
+export default class CreateUsers implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values({
+        first_name: process.env.USER_ADMIN_FIRST_NAME,
+        last_name: process.env.USER_ADMIN_LAST_NAME,
+        email: process.env.USER_ADMIN_EMAIL,
+        password: encryptPassword(process.env.USER_ADMIN_PASSWORD as string),
+        verified_email: true,
+        admin: true
+      })
+      .execute()
+  }
+}

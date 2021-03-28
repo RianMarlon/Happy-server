d79979484c6a0229e6e8014c53941e706c3bf56d
Rian Marlon <rianmarlon20@yahoo.com>
2021-03-27 22:13:09 -0300

    Alterando o emailService.ts da pasta modules do projeto server

--- src/modules/emailService.ts
+++ src/modules/emailService.ts
@@ -1,25 +1,44 @@
-import nodemailer from 'nodemailer';
-import path from 'path';
-import 'dotenv/config';
+import nodemailer, { Transporter } from 'nodemailer';
+import fs from 'fs';
+import handlebers from 'handlebars';
 
-const hbs = require('nodemailer-express-handlebars');
+class SendMailService {
+  private client: Transporter
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_SERVICE_HOST,
+        port: Number(process.env.MAIL_SERVICE_PORT),
+        auth: {
+          user: process.env.MAIL_SERVICE_USER,
+          pass: process.env.MAIL_SERVICE_PASS
+        }
+      });
 
-const transport = nodemailer.createTransport({
-  host: process.env.MAIL_SERVICE_HOST,
-  port: Number(process.env.MAIL_SERVICE_PORT),
-  auth: {
-    user: process.env.MAIL_SERVICE_USER,
-    pass: process.env.MAIL_SERVICE_PASS
+      this.client = transporter;
+    });
   }
-});
 
-transport.use('compile', hbs({
-  viewEngine: {
-    defaultLayout: undefined,
-    partialsDir: path.resolve('./src/resources/emailTemplates/')
-  },
-  viewPath: path.resolve('./src/resources/emailTemplates/'),
-  extName: '.html'
-}));
+  async execute(
+    to: string, 
+    from: string, 
+    subject: string, 
+    variables: object, 
+    path: string,
+  ) {
+    const templateFileContent = fs.readFileSync(path).toString('utf8');
 
-export default transport;
+    const mailTemplateParse = handlebers.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from,
+      subject,
+      html
+    });
+  }
+}
+
+export default new SendMailService();

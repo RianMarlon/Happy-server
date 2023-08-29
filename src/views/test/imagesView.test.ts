cb4fd4c4f46bbe90795d952d890c92745cc9ddd7
Rian Marlon <rianmarlon20@yahoo.com>
2023-08-28 22:21:33 -0300

    test: add unit tests for views

---
+++ src/views/test/imagesView.test.ts
@@ -0,0 +1,26 @@
+import Image from '../../models/Image';
+import imagesView from '../imagesView';
+
+describe('imagesView Tests', () => {
+  describe('renderMany', () => {
+    it('should return correct structure', async () => {
+      expect(
+        imagesView.renderMany([
+          { id: 1, path: 'teste-1.png' },
+          { id: 2, path: 'teste-2.png' },
+        ] as Image[])
+      ).toEqual([
+        { id: 1, url: `${process.env.API_URL}/uploads/teste-1.png` },
+        { id: 2, url: `${process.env.API_URL}/uploads/teste-2.png` },
+      ]);
+    });
+  });
+
+  describe('render', () => {
+    it('should return correct structure', async () => {
+      expect(
+        imagesView.render({ id: 1, path: 'teste-1.png' } as Image)
+      ).toEqual({ id: 1, url: `${process.env.API_URL}/uploads/teste-1.png` });
+    });
+  });
+});
---
+++ src/views/test/orphanagesView.test.ts
@@ -0,0 +1,168 @@
+import Orphanage from '../../models/Orphanage';
+import orphanagesView from '../orphanagesView';
+
+describe('orphanagesView Tests', () => {
+  describe('renderMany', () => {
+    it('should return correct structure', async () => {
+      expect(
+        orphanagesView.renderMany([
+          {
+            id: 1,
+            name: 'Teste',
+            latitude: -5.101444,
+            longitude: -38.369682,
+            about: 'Teste',
+            whatsapp: '9999999999',
+            instructions: 'Teste',
+            open_from: 1020,
+            open_until: 1140,
+            open_on_weekends: true,
+            confirmed: true,
+            images: [
+              {
+                id: 1,
+                path: 'teste.png',
+              },
+            ],
+          },
+          {
+            id: 2,
+            name: 'Teste 2',
+            latitude: -5.096411,
+            longitude: -38.368701,
+            about: 'Teste 2',
+            whatsapp: '9999999999',
+            instructions: 'Teste',
+            open_from: 540,
+            open_until: 780,
+            open_on_weekends: true,
+            confirmed: true,
+            images: [
+              {
+                id: 1,
+                path: 'teste.png',
+              },
+            ],
+          },
+          {
+            id: 3,
+            name: 'Teste 3',
+            latitude: -5.095159,
+            longitude: -38.371198,
+            about: 'Teste 3',
+            whatsapp: '9999999999',
+            instructions: 'Teste',
+            open_from: 900,
+            open_until: 1140,
+            open_on_weekends: true,
+            confirmed: true,
+            images: [
+              {
+                id: 1,
+                path: 'teste.png',
+              },
+            ],
+          },
+        ] as Orphanage[])
+      ).toEqual([
+        {
+          about: 'Teste',
+          id: 1,
+          images: [
+            {
+              id: 1,
+              url: 'http://192.168.1.4:3333/uploads/teste.png',
+            },
+          ],
+          instructions: 'Teste',
+          latitude: -5.101444,
+          longitude: -38.369682,
+          name: 'Teste',
+          open_from: '17:00',
+          open_on_weekends: true,
+          open_until: '19:00',
+          whatsapp: '9999999999',
+        },
+        {
+          about: 'Teste 2',
+          id: 2,
+          images: [
+            {
+              id: 1,
+              url: 'http://192.168.1.4:3333/uploads/teste.png',
+            },
+          ],
+          instructions: 'Teste',
+          latitude: -5.096411,
+          longitude: -38.368701,
+          name: 'Teste 2',
+          open_from: '09:00',
+          open_on_weekends: true,
+          open_until: '13:00',
+          whatsapp: '9999999999',
+        },
+        {
+          about: 'Teste 3',
+          id: 3,
+          images: [
+            {
+              id: 1,
+              url: 'http://192.168.1.4:3333/uploads/teste.png',
+            },
+          ],
+          instructions: 'Teste',
+          latitude: -5.095159,
+          longitude: -38.371198,
+          name: 'Teste 3',
+          open_from: '15:00',
+          open_on_weekends: true,
+          open_until: '19:00',
+          whatsapp: '9999999999',
+        },
+      ]);
+    });
+  });
+
+  describe('render', () => {
+    it('should return correct structure', async () => {
+      expect(
+        orphanagesView.render({
+          id: 1,
+          name: 'Teste',
+          latitude: -5.101444,
+          longitude: -38.369682,
+          about: 'Teste',
+          whatsapp: '9999999999',
+          instructions: 'Teste',
+          open_from: 1020,
+          open_until: 1140,
+          open_on_weekends: true,
+          confirmed: true,
+          images: [
+            {
+              id: 1,
+              path: 'teste.png',
+            },
+          ],
+        } as Orphanage)
+      ).toEqual({
+        about: 'Teste',
+        id: 1,
+        images: [
+          {
+            id: 1,
+            url: 'http://192.168.1.4:3333/uploads/teste.png',
+          },
+        ],
+        instructions: 'Teste',
+        latitude: -5.101444,
+        longitude: -38.369682,
+        name: 'Teste',
+        open_from: '17:00',
+        open_on_weekends: true,
+        open_until: '19:00',
+        whatsapp: '9999999999',
+      });
+    });
+  });
+});
